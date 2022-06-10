4a2b21027c613b748eb56a992ba74641f37e7265
Yoku <yoku@LAPTOP-82UIBLK4>
2022-06-10 18:40:52 +0600

    boooot finally

--- bot.py
+++ bot.py
@@ -1,8 +1,27 @@
 import requests
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-
+import db
+data
 API_TOKEN = '5509187287:AAE8EXqIEGsXCCzBJ-8GbnHeS49UGMRKVUQ'
 URL = f'https://api.telegram.org/bot{API_TOKEN}/getMe'
 
-logging.BasicConf(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi\nI'am Housing bot\nPowerd by aiogram.")
+
+
+@dp.message_handler(commands=['/new'])
+async def echo(message: types.Message):
+    houses = db.read_from_db()
+    await message.reply()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
--- db.py
+++ db.py
@@ -3,14 +3,38 @@ import os
 import json
 import psycopg2
 import psycopg2.errorcodes
+from collections import namedtuple
+from typing import NamedTuple
+from dataclasses import dataclass
+
+
+class DataFromDB(NamedTuple):
+    db_id: int
+    ad_id: int
+    ad_type: str
+    ad_name: str
+    price: int
+    address_title: str
+    country: str
+    region: str
+    city: str
+    street: str
+    house_num: str
+    rooms: int
+    owners_name: str
+    url: str
+    added_date: list
+    photo_url: list
+
 
 conn = psycopg2.connect(
         host='localhost',
         dbname='psql_test',
-        user='test_user',
-        password='testpass123'
+        user='postgres',
+        password='postgres'
     )
 cur = conn.cursor()
+
 
 class SqlQuerys:
     get_all_ad_ids = "SELECT houses.ad_id FROM houses;"
@@ -28,9 +52,11 @@ def _write_into_houses(raw_json):
     table_name = 'houses'
     cur.execute(SqlQuerys.get_all_ad_ids)
     db_ad_ids = cur.fetchall()
-    for ad_id in raw_json:
-        fields = 'ad_id, type, ad_name, price, address_title, country, region,\
+    fields = 'ad_id, type, ad_name, price, address_title, country, region,\
 city, street, house_num, rooms, owners_name, url, added_date' 
+    for ad_id in raw_json:
+        if ad_id in db_ad_ids:
+            continue
         values = [] 
         values.append(ad_id)
         values.append(raw_json[ad_id][0]['type'])
@@ -61,7 +87,11 @@ def _write_into_photos(raw_json):
 def _write_into_maps(raw_json):
     table_name = 'map_data'
     fields = 'ad_id, lat, lon, zoom'
+    cur.execute(SqlQuerys.get_all_ad_ids)
+    db_ad_ids = cur.fetchall()
     for ad_id in raw_json:
+        if ad_id in db_ad_ids:
+            continue
         values = []
         raw_json[ad_id][0]['map']
         values.append(ad_id)
@@ -70,23 +100,41 @@ def _write_into_maps(raw_json):
         values.append(raw_json[ad_id][0]['map']['zoom'])
         _write_into_table(table_name, f"({fields})", tuple(values))
 
-def read_last_n_from_db(n):
-    return cur.fetchall()[:n]
+def _convert_sql_to_named_tuple(data):
+    d = data
+    return DataFromDB(
+        db_id=d[0], ad_id=d[1], ad_type=d[2], ad_name=d[3], price=d[4],\
+        address_title=d[5], country=d[6], region=d[7], city=d[8], street=d[9],\
+        house_num=d[10], rooms=d[11], owners_name=d[12], url=d[13],\
+        added_date=d[14], photo_url=d[15])
 
-def read_last_data_from_database():
-    return cur.fetchone()
 
-def read_from_db():
-    cur.execute("""
-        SELECT houses.*, ARRAY_AGG(photos.photo_url) as photos FROM houses
-        LEFT JOIN photos ON houses.ad_id = photos.ad_id
-        GROUP BY houses.ad_id ORDER BY houses.added_date DESC;""")
-    return cur.fetchall()
-
+    
 def _write_into_table(table, fields, values):
     print(f"INSERT INTO {table.lower()} {fields} VALUES {values}")
     cur.execute(f"INSERT INTO  {table.lower()} {fields} VALUES {values}")
     conn.commit()
+
+def fetch_all_from_db():
+    cur.execute("""
+    SELECT houses.*, ARRAY_AGG(photos.photo_url) as photos FROM houses
+    LEFT JOIN photos ON houses.ad_id = photos.ad_id
+    GROUP BY houses.ad_id ORDER BY houses.added_date DESC;""")
+    data = cur.fetchall()
+    result = []
+    for house in data:
+        result.append(_convert_sql_to_named_tuple(house))
+    return result
+
+def fetch_last_n_from_db(*n):
+    return cur.fetchall()[:n]
+
+def fetch_one_from_db():
+    return cur.fetchone()
+
+def read_from_map_data():
+    cur.execute("SELECT * FROM map_data;")
+    return cur.fetchone()
 
 def insert_into_database(raw_json):
     try:
@@ -101,4 +149,6 @@ def insert_into_database(raw_json):
 
 _init_database()
 if __name__ == '__main__':
-    pass
+    data = fetch_all_from_db()
+    for i in data:
+        print(i.price)
